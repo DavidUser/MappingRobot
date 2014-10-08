f5c9c43b86b4194a02af42664187d88133de023b
David Kennedy <daviduser@me.com>
2014-10-08 19:59:38 -0300

    prototype of class Snapshot (concrete class)

---
+++ src/Model/Snapshot.java
@@ -0,0 +1,15 @@
+class Snapshot {
+	public int getDegreeMeasure(int measure) {
+		//TODO implements
+	}
+
+	public void buildString(String string) {
+		//TODO implements
+	}
+	public void buildXml(String filePath) {
+		//TODO implements
+	}
+	public void buildcsv(string filePath) {	
+		//TODO implements
+	}
+}
