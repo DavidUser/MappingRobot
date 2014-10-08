4bfe1149f1e3f22b0fe6540fee6b10ab8fa54bc6
David Kennedy <daviduser@me.com>
2014-10-08 19:31:19 -0300

    prototype of class Logger

---
+++ src/Model/Logger.java
@@ -0,0 +1,21 @@
+class Logger {
+	public void registerLogin(User user) {
+		//TODO implements
+	}
+
+	public void registerSetMapPrecision(int precision) {
+		//TODO implements
+	}
+
+	public void setMapArea(int height, int width) {
+		//TODO implements
+	}
+
+	public void mapTakeNewPoint(Position position) {
+		//TODO implements
+	}
+
+	public void analyserTakeDecision(String decision) {
+		//TODO implements
+	}
+}
