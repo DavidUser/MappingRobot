c59629b54ba9b90c76c5fddb2e4c0045753e57ff
David Kennedy <daviduser@me.com>
2014-10-08 19:11:26 -0300

    prototype of class System

---
+++ src/Model/System.java
@@ -0,0 +1,21 @@
+class System {
+	public boolean login(User user) {
+		//TODO implements
+	}
+
+	public void setMapPrecision(int precision) {
+		//TODO implements
+	}
+
+	public void setMapArea(int height, int width) {
+		//TODO implements
+	}
+	
+	public void stopService() {
+		//TODO implements
+	}
+
+	public void startService() {
+		//TODO implements
+	}
+}
