29eedd869087634d3fe0609621608d8cba22b11f
David Kennedy <daviduser@me.com>
2014-10-13 20:37:48 -0300

    Implements Singleton to Analyser

--- src/Servidor/Model/Analyser.java
+++ src/Servidor/Model/Analyser.java
@@ -3,10 +3,17 @@ package Servidor.Model;
 import Embarcado.logic.Position;
 
 public class Analyser {
+	private Analyser instance;
 	public Communication communication;
 
-	public Analyser() {
-		throw new UnsupportedOperationException();
+	private Analyser() {
+		communication = new Communication();
+	}
+
+	public Analyser getInstance() {
+		if (instance == null)
+			instance = new Analyser();
+		return instance;
 	}
 
 	public void takeDecision() {
