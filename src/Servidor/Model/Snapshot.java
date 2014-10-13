3aefac51b44c1abca4ad228ab072f94729901b0e
David Kennedy <daviduser@me.com>
2014-10-13 19:16:11 -0300

    Add data members to Snapshot

--- src/Servidor/Model/Snapshot.java
+++ src/Servidor/Model/Snapshot.java
@@ -1,6 +1,12 @@
 package Servidor.Model;
 
 public class Snapshot {
+	private int[] measures; //TODO improve container to better space cost or performace cost, possible use a vector
+
+	public Snapshot(int [] measures) { //expected 360 measures
+		//TODO verify size of 360 measures else throw error
+		this.measures = measures;
+	}
 
 	public int getDegreeMeasure(int measure) {
 		throw new UnsupportedOperationException();
