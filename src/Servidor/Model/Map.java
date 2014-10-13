39ff1150c51c0ad830e94f4a5d5550038f58249b
David Kennedy <daviduser@me.com>
2014-10-13 20:20:03 -0300

    First implementation of System and Map

--- src/Servidor/Model/Map.java
+++ src/Servidor/Model/Map.java
@@ -2,9 +2,16 @@ package Servidor.Model;
 
 import Embarcado.logic.Position;
 
+//TODO improve class to set precision of a existing map without lose reads
 public class Map {
-	public Map(int height, int width) {
-		throw new UnsupportedOperationException();
+	private int height, width;
+	private int scale; //distance for a point to another growing in height or width, in centimeters
+
+	public Map(int height, int width, int scale) {
+		setHeight(height);
+		setWidth(width);
+		if (scale > 0)
+			this.scale = scale; //TODO else throw a error
 	}
 
 	public void takeNewPoint(Position position, Snapshot snapshot) {
@@ -13,5 +20,25 @@ public class Map {
 
 	public void buildObjects() {
 		throw new UnsupportedOperationException();
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getScale() {
+		return this.scale;
 	}
 }
--- src/Servidor/Model/System.java
+++ src/Servidor/Model/System.java
@@ -3,8 +3,11 @@ package Servidor.Model;
 public class System {
 	public Analyser analyser;
 	public Map map;
+	public boolean logged;
 
 	public System() {
+		map = null;
+		logged = false;
 		throw new UnsupportedOperationException();
 	}
 
@@ -13,18 +16,31 @@ public class System {
 	}
 
 	public void setMapPrecision(int precision) {
+		if (map != null)
+			System.err.println("set a precision of map, erase all content of map");
+		map = new Map(1, 1, precision);
+		//TODO send message to microcontroller
 		throw new UnsupportedOperationException();
 	}
 
 	public void setMapArea(int height, int width) {
+		if (map == null) {
+			map = new Map(height, width, 1);
+		} else {
+			map.setHeight(height);
+			map.setWidth(width);
+		}
+		//TODO send message to microcontroller
 		throw new UnsupportedOperationException();
 	}
 
 	public void stopService() {
+		//TODO send message to microcontroller
 		throw new UnsupportedOperationException();
 	}
 
 	public void startService() {
+		//TODO send message to microcontroller
 		throw new UnsupportedOperationException();
 	}
 }
